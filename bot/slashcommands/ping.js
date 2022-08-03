1dbd96e7babbe3101e965c0135c427d151dd113d
Sylicium <96630108+Sylicium@users.noreply.github.com>
2022-08-03 02:07:29 +0200

    Update .gitignore and edited /ping command
    Update .gitignore and edited /ping command with better response

--- bot/slashcommands/ping.js
+++ bot/slashcommands/ping.js
@@ -10,7 +10,7 @@ module.exports = {
     commandInformations: {
         commandDatas: {
             name: "ping",
-            description: "Pong!",
+            description: "Renvoie la latence du bot et de l'api.",
             dmPermission: false,
             type: Discord.ApplicationCommandType.ChatInput,
             options: []
@@ -27,9 +27,22 @@ module.exports = {
         hideOnHelp: false
     },
     execute: async (bot, interaction, data, a,b,c,d,e,f,g,h) => {
-        interaction.reply({
-            content: "pong !",
-            ephemeral: true
-        })
+        
+        let latency = Date.now() - interaction.createdTimestamp
+
+        let ephemeral = false
+        
+        if(latency < 0) {
+            return interaction.reply({ephemeral:ephemeral, content:`**:ping_pong: Latency is ${latency}ms. API Latency is ${Math.round(bot.ws.ping)}ms. ❔**`})
+        } else if(latency < 400) {
+            return interaction.reply({ephemeral:ephemeral, content:`**:ping_pong: Latency is ${latency}ms. API Latency is ${Math.round(bot.ws.ping)}ms. :green_circle:**`})
+        } else if(latency < 700) {
+            return interaction.reply({ephemeral:ephemeral, content:`**:ping_pong: Latency is ${latency}ms. API Latency is ${Math.round(bot.ws.ping)}ms. :orange_circle:**`})
+        } else if(latency < 1000) {
+            return interaction.reply({ephemeral:ephemeral, content:`**:ping_pong: Latency is ${latency}ms. API Latency is ${Math.round(bot.ws.ping)}ms. :red_circle:**`})
+        } else {
+            return interaction.reply({ephemeral:ephemeral, content:`**:ping_pong: Latency is ${latency}ms. API Latency is ${Math.round(bot.ws.ping)}ms. ⚠**`})
+        }
+
     }
 }
